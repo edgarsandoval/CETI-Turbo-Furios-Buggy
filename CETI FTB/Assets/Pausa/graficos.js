c40d8bacb85bb026f7b2cfbfbe472f14555d43ff
Adrian Sanchez <sgzadrian@gmail.com>
2016-03-29 22:39:53 -0600

    Corregidos algunos detalles

---
+++ CETI FTB/Assets/Pausa/graficos.js
@@ -0,0 +1,24 @@
+﻿public function Graficos1()
+{
+	QualitySettings.currentLevel = QualityLevel.Fastest;
+}
+public function Graficos2()
+{
+	QualitySettings.currentLevel = QualityLevel.Fast;
+}
+public function Graficos3()
+{
+	QualitySettings.currentLevel = QualityLevel.Simple;
+}
+public function Graficos4()
+{
+	QualitySettings.currentLevel = QualityLevel.Good;
+}
+public function Graficos5()
+{
+	QualitySettings.currentLevel = QualityLevel.Beautiful;
+}
+public function Graficos6()
+{
+	QualitySettings.currentLevel = QualityLevel.Fantastic;
+}
