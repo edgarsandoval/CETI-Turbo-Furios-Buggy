02bf81a599de61b8c67aa2bab62182200303b3f1
Adrian Sanchez <sgzadrian@gmail.com>
2016-05-30 20:37:24 -0500

    Se agrego personaje (por fin) y creo prefab

--- CETI FTB/Assets/Armas/Trigger.cs
+++ CETI FTB/Assets/Armas/Trigger.cs
@@ -2,21 +2,35 @@
 using System.Collections;
 
 public class Trigger : MonoBehaviour {
-	private float timer = 10.0f;
+
+	public GameObject[] Armas; 
+	public static int actual = 0; // 0 = ninguna
 
 	void Start () {
 	
 	}
 	
 	void Update () {
-		//Espera de 1 segundo
-		timer--;
+		//if (Input.GetKeyDown ("space"))
+			//soltar ();
 	}
 
 	void OnTriggerEnter (Collider Check) {
 		if (Check.tag == "Player") {
-			this.enabled = false; //Desactiva la caja? :v
-			timer = 10.0f;
+			this.gameObject.SetActive (false);
+			actual = Random.Range (1, Armas.Length);
 		}
 	}
+	/*
+	public void soltar () {
+		GameObject carro = CargarCarro.getCarro();
+		GameObject cactus = Instantiate (Armas [actual], carro.transform.position, carro.transform.rotation) as GameObject;
+		actual = 0;
+	}
+
+	public void aventar () {
+		GameObject carro = CargarCarro.getCarro();
+		GameObject lanzar = Instantiate (Armas [Trigger.actual], carro.transform.position, carro.transform.rotation) as GameObject;
+		// ? :c lanzar.rigidbody.AddForce (transform.forward * 2000 * 3);
+	}*/
 }
---
+++ CETI FTB/Assets/Solo/Scripts/Poder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Poder : MonoBehaviour {
+	public Text txt;
+
+	void Start () {
+	}
+
+	void Update () {
+		switch (Trigger.actual) {
+		case 0:
+			txt.text = "";
+			break;
+		case 1:
+			txt.text = "Micro";
+			break;
+		case 2:
+			txt.text = "HDD";
+			break;
+		case 3:
+			txt.text = "Sancho";
+			break;
+		case 4:
+			txt.text = "Aifon";
+			break;
+		}
+	}
+}
