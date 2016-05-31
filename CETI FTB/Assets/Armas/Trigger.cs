feee2728477daf21a6035c1c6539c1263a92cb1c
Adrian Sanchez <sgzadrian@gmail.com>
2016-05-30 23:16:18 -0500

    :c pinches armas culeras

--- CETI FTB/Assets/Armas/Trigger.cs
+++ CETI FTB/Assets/Armas/Trigger.cs
@@ -4,30 +4,31 @@ using System.Collections;
 public class Trigger : MonoBehaviour {
 
 	public GameObject[] Armas; 
+	public GameObject PlayerPrefab;
 	public static int actual = 0; // 0 = ninguna
 
 	void Start () {
-	
 	}
 	
 	void Update () {
-		//if (Input.GetKeyDown ("space"))
-			//soltar ();
+		if (Input.GetKeyDown ("space") && actual != 0)
+			soltar ();
 	}
 
 	void OnTriggerEnter (Collider Check) {
-		if (Check.tag == "Player") {
+		if (Check.tag == "Player" || Check.tag == "IA") {
 			this.gameObject.SetActive (false);
 			actual = Random.Range (1, Armas.Length);
 		}
 	}
-	/*
+
 	public void soltar () {
-		GameObject carro = CargarCarro.getCarro();
-		GameObject cactus = Instantiate (Armas [actual], carro.transform.position, carro.transform.rotation) as GameObject;
+		GameObject arma = Instantiate (Armas [2], PlayerPrefab.transform.position, Quaternion.identity) as GameObject;
+		arma.SetActive (true);
+		arma.AddComponent<MeshFilter> ();
 		actual = 0;
 	}
-
+	/*
 	public void aventar () {
 		GameObject carro = CargarCarro.getCarro();
 		GameObject lanzar = Instantiate (Armas [Trigger.actual], carro.transform.position, carro.transform.rotation) as GameObject;
