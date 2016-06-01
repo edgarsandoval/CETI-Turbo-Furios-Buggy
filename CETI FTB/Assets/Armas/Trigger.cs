7d0c9373d9f4208c562335bf90a5625c1abd2f6f
Edgar Sandoval <edgarsandovalavalos@hotmail.com>
2016-06-01 00:25:23 -0500

    Añadido un pack de texturas. :v

--- CETI FTB/Assets/Armas/Trigger.cs
+++ CETI FTB/Assets/Armas/Trigger.cs
@@ -5,47 +5,25 @@ public class Trigger : MonoBehaviour {
 
 	public GameObject[] Armas; 
 	public GameObject PlayerPrefab;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	public static int actual = -1; // -1 = ninguna
-=======
-=======
->>>>>>> feee2728477daf21a6035c1c6539c1263a92cb1c
-	public static int actual = 0; // 0 = ninguna
->>>>>>> feee2728477daf21a6035c1c6539c1263a92cb1c
 
 	void Start () {
 	}
 	
 	void Update () {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		if (Input.GetKeyDown ("space") && actual >= 0) {
-			/*if (actual == 2)
-				soltar ();
-			else
-				aventar ();*/
+
+		if (Input.GetKeyDown ("space") && actual != -1)
 			soltar ();
-		}
-=======
-		if (Input.GetKeyDown ("space") && actual != 0)
-			soltar ();
->>>>>>> feee2728477daf21a6035c1c6539c1263a92cb1c
-=======
-		if (Input.GetKeyDown ("space") && actual != 0)
-			soltar ();
->>>>>>> feee2728477daf21a6035c1c6539c1263a92cb1c
 	}
 
 	void OnTriggerEnter (Collider Check) {
 		if (Check.tag == "Player" || Check.tag == "IA") {
 			this.gameObject.SetActive (false);
 			actual = Random.Range (0, Armas.Length);
-			Debug.Log (actual);
 		}
 	}
 
-<<<<<<< HEAD
 	public static string getActual() {
 		switch (actual) {
 		default:
@@ -69,19 +47,10 @@ public class Trigger : MonoBehaviour {
 	}
 
 	public void soltar () {
-<<<<<<< HEAD
 		GameObject arma = Instantiate (Armas [actual], PlayerPrefab.transform.position, Quaternion.identity) as GameObject;
 		arma.SetActive (true);
 		actual = -1;
-=======
-=======
-	public void soltar () {
->>>>>>> feee2728477daf21a6035c1c6539c1263a92cb1c
-		GameObject arma = Instantiate (Armas [2], PlayerPrefab.transform.position, Quaternion.identity) as GameObject;
-		arma.SetActive (true);
-		arma.AddComponent<MeshFilter> ();
-		actual = 0;
->>>>>>> feee2728477daf21a6035c1c6539c1263a92cb1c
+
 	}
 	/*
 	public void aventar () {
@@ -92,4 +61,5 @@ public class Trigger : MonoBehaviour {
 		actual = -1;
 		// ? :c lanzar.rigidbody.AddForce (transform.forward * 2000 * 3);
 	}
+	*/
 }
